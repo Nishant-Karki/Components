a49594dc0ab1f9dff63517f3431d5819d5784992
Sujan Paudel <sujan2000paudel@gmail.com>
2021-01-03 10:15:06 +0545

    Add Navbar Component by Sujan

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ import Nav1 from './Navbars/Nav1';
 import Footer from './Footer/Footer';
 import Nav2 from './Navbars/Nav2';
 import Nav3 from './Navbars/Nav3';
+import NavbarSujanMaterialUI from './Navbars/NavbarSujanMaterialUI';
+import NavbarSujanReactStrap from './Navbars/NavbarSujanReactStrap';
 
 export default function Home() {
   return (
@@ -16,6 +18,8 @@ export default function Home() {
       <Input1 />
       <Nav2 />
       <Nav3 />
+      <NavbarSujanMaterialUI />
+      <NavbarSujanReactStrap />
       <Footer />
     </div>
   );
